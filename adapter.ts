dbe898e1e675addd6bf889a2c1ed8b83f46580a7
Joshua Parkin <joshuadparkin@gmail.com>
2023-05-02 14:11:44 +0100

    make language bundle deno compatable

--- adapter.ts
+++ adapter.ts
@@ -1,48 +1,46 @@
-import type { Address, Expression, ExpressionAdapter, PublicSharing, LanguageContext, AgentService } from "@perspect3vism/ad4m";
-import type { IPFS } from "ipfs-core-types";
-import path from "path";
-import fs from "fs";
+import type { Address, Expression, ExpressionAdapter, PublicSharing, LanguageContext, AgentService } from "https://esm.sh/@perspect3vism/ad4m@0.3.4";
+import { join } from "https://deno.land/std@0.184.0/path/mod.ts";
+import { exists } from "https://deno.land/std@0.184.0/fs/mod.ts";
 
 class SharedPerspectivePutAdapter implements PublicSharing {
   #agent: AgentService;
-  #IPFS: IPFS
   #storagePath: string;
 
   constructor(context: LanguageContext) {
     this.#agent = context.agent;
-    this.#IPFS = context.IPFS;
+    //@ts-ignore
     if ("storagePath" in context.customSettings) { this.#storagePath = context.customSettings["storagePath"] } else { this.#storagePath = "./tst-tmp/" };
   }
 
   async createPublic(neighbourhood: object): Promise<Address> {
     const expression = this.#agent.createSignedExpression(neighbourhood);
     const content = JSON.stringify(expression);
-    const result = await this.#IPFS.add({ content }, { onlyHash: true });
-    const address = result.cid.toString();
+    // @ts-ignore
+    const address = UTILS.hash(content);
 
-    const neighbourhoodPath = path.join(this.#storagePath, `neighbourhood-${address}.json`);
+    const neighbourhoodPath = join(this.#storagePath, `neighbourhood-${address}.json`);
     console.log("Writing neighbourhood with path: ", neighbourhoodPath);
-    fs.writeFileSync(neighbourhoodPath, content);
+    Deno.writeTextFileSync(neighbourhoodPath, content);
 
     return address
   }
 }
 
 export default class Adapter implements ExpressionAdapter {
-  #IPFS: IPFS
   #storagePath: string;
 
   putAdapter: PublicSharing;
 
   constructor(context: LanguageContext) {
     this.putAdapter = new SharedPerspectivePutAdapter(context);
+    //@ts-ignore
     if ("storagePath" in context.customSettings) { this.#storagePath = context.customSettings["storagePath"] } else { this.#storagePath = "./tst-tmp/" };
   }
 
   async get(address: Address): Promise<Expression> {
-    const neighbourhoodPath = path.join(this.#storagePath, `neighbourhood-${address}.json`)
-    if (fs.existsSync(neighbourhoodPath)) {
-      const neighbourhood = JSON.parse(fs.readFileSync(neighbourhoodPath).toString());
+    const neighbourhoodPath = join(this.#storagePath, `neighbourhood-${address}.json`)
+    if (await exists(neighbourhoodPath)) {
+      const neighbourhood = JSON.parse(Deno.readTextFileSync(neighbourhoodPath).toString());
       console.log("Found neighbourhood: ", neighbourhood);
       return neighbourhood
     } else {
--- index.ts
+++ index.ts
@@ -1,32 +1,18 @@
-import type { Address, Language, LanguageContext, ExpressionUI, Interaction } from "@perspect3vism/ad4m";
-import Adapter from "./adapter";
+import type { Address, Language, LanguageContext, ExpressionUI, Interaction } from "https://esm.sh/@perspect3vism/ad4m@0.3.4";
+import Adapter from "./adapter.ts";
 
 function interactions(expression: Address): Interaction[] {
   return [];
 }
 
-export class UI implements ExpressionUI {
-  icon(): string {
-    return "";
-  }
-
-  constructorIcon(): string {
-    return "";
-  }
-}
-
 export const name = "neighbourhood-store";
 
 export default async function create(context: LanguageContext): Promise<Language> {
-  //const Holochain = context.Holochain as HolochainLanguageDelegate;
-  //await Holochain.registerDNAs([{ file: DNA, nick: DNA_NICK }]);
   const expressionAdapter = new Adapter(context);
-  //const expressionUI = new UI();
 
   return {
     name,
     expressionAdapter,
-    //expressionUI,
     interactions,
   } as Language;
 }
